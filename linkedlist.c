68a4e45bb31665abf187c281832cc6d2919ccece
Sanderson40 <sanderson40@stuy.edu>
2023-10-23 17:49:35 -0400

    Added previously coded linked list and adjusted to have artist name

--- linkedlist.c
+++ linkedlist.c
@@ -1,29 +1,43 @@
-int mystrlen( char *s ){\
-   for(int i= 0; 1; i++){
-       if(s[i]=='\0'){
-           return i;
-       }
-   }
-   //return 1;
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedlist.h"
+#include <string.h>
 
+int print(struct node s, int n){
+    printf("[%d] Song %s Artist %s\n", n, s.name, s.artist);
+}
+
+struct node* new(char n[], char Artist[], struct node *next){
+    
+    struct node *a=  malloc(208);
+    strcpy(a->name, n);
+    strcpy(a->artist, Artist);
+    a->next = next;
+    return a;
 
 }
 
-int mystrcmp( char *s1, char *s2 ){
-   for(int i = 0; i<=(mystrlen(s2) > mystrlen(s1) ? mystrlen(s1) : mystrlen(s2)); i++){
-       if(s1[i]==s2[i]){}
-       else{return ((int) s1[i])-((int) s2[i]);}
-   }
-   return 0;
-
-
-
+struct node * insert_front(struct node * next, char* n, char* a){
+    new(n, a, next);
 
 }
 
+void print_list(struct node * front){
+    for(int i = 0; !front == '\0'; i++){
+        print(*front, i);
+        front = front->next;
+    }
 
-char* mystrcpy(char *dest, char *source){
-   char* orig = dest;
-   while(*dest++ = *source++);
-   return orig;
+}
+
+struct node * free_list(struct node * front){
+    struct node * next;
+    while(!front=='\0'){
+        next =  front->next;
+        free(front);
+        front = next;
+
+    }
+    return front;
+
 }
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,9 @@
 int mystrcmp( char *s1, char *s2 );
 int mystrlen( char *s );
 char* mystrcpy(char *dest, char *source);
+struct node { char name[100]; char artist[100]; struct node *next;};
+int print(struct node s, int);
+struct node* new(char n[], char a[], struct node *next);
+struct node * insert_front(struct node *next , char* n, char* a);
+void print_list(struct node * front);
+struct node * free_list(struct node *front );
