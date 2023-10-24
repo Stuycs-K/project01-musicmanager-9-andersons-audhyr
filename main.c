8bb5816ed696518737a7d2f745a31bfc645962a4
Spencer Anderson <sanderson40@stuy.edu>
2023-10-24 14:45:20 -0400

    added some test cases

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+#include "linkedlist.h"
+#include <stdio.h>
+#include <stdlib.h>
 int main(){
-
+    struct node * front = insert_front( '\0',"Bohemian Rhapsody", "Queen");
+    front = insert_front( front,"Build Me Up Buttercup", "The Foundations");
+    print_list(front);
+    struct node * mysong=songpoint(front, "Bohemian Rhapsody", "Queen");
+    printf("%s %s\n", mysong->song, mysong->artist);
     
 }
