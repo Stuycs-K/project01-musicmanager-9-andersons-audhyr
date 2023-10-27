4273147c8438a2e7a07b45860aad55cc5da16d6e
Riasat Audhy <raudhy40@stuy.edu>
2023-10-27 14:43:29 -0400

    started better tests

--- main.c
+++ main.c
@@ -3,7 +3,64 @@
 #include <stdlib.h>
 #include <time.h>
 #include "library.h"
+
 int main(){
+    srand( time(NULL) );
+    char artists[10][35] = {"Apple Metal", "Apple Metal", "Apple Metal", "Megaapple", "Megaapple", "Megaapple", "Augustus's Poor Apple", "Japan Panic", "Japan Panic", "Japan Panic"};
+    char songs[10][35] = {"Classic Misery", "Burning Revolution", "Hang Loose Dreams", "Without You, I Can't Feel Silence", "Honey, You Are My Dawing", "Deep Festival", "Afraid Of Blame", "Funny Train", "Fusion Light", "Complete Bird"};
+
+    struct node* playlist = NULL;
+
+    printf("Testing insert nodes in order (to generate an ordered song list) AND print whole list:\n");
+    for(int i = 0; i < 10; i++) playlist = insertOrder(playlist, new(songs[i], artists[i], NULL));
+    print_list(playlist);
+    printf("-----------\n");
+
+    printf("Testing insert node to front:\n");
+    playlist = insert_front(playlist, "song", "added to the front");
+    print_list(playlist);
+    printf("-----------\n");
+
+    printf("Testing find pointer to node with given song and printing the resulting pointer (song:'Burning Revolution', artist:'Apple Metal'):\n" );
+    print(songpoint(playlist, "Burning Revolution", "Apple Metal"), 0);
+    printf("-----------\n");
+
+    // printf("Testing find pointer to node with given song and printing the resulting pointer (song:'Burning Revoluaation', artist:'Apple Metal'): Song doesn't exist so expecting nothing\n" );
+    // print(songpoint(playlist, "Burning Revoluaation", "Apple Metal"), 0);  //CURRENTLY SEGFAULTS
+    // printf("-----------\n");
+
+    printf("Testing & printing pointer to first song by artist 'Megaapple':\n");
+    print(artistpoint(playlist, "Megaapple"), 0);
+    printf("-----------\n");
+
+    // printf("Testing & printing pointer to first song by artist 'Megaapplllle': Song doesn't exist so expecting nothing\n" );
+    // print(artistpoint(playlist, "Appllle Metal"), 0);  //CURRENTLY SEGFAULTS
+    // printf("-----------\n");
+
+    // print(NULL, 0);  //SEGFAULTS
+
+    printf("Testing & printing pointer to random song in playlist:\n");
+    print(randomSong(playlist), 0);
+    printf("-----------\n");
+
+    printf("Testing remove single node with song: 'Hang Loose Dreams' artist: 'Apple Metal' and printing the resulting list:\n");
+    playlist = removeSong(playlist, "Hang Loose Dreams", "Apple Metal");
+    print_list(playlist);
+    printf("-----------\n");
+
+    printf("Testing remove single node with nonexistant song and printing the resulting list:(expecting no change)\n");
+    playlist = removeSong(playlist, "doesnt exist", "Apple Metal");
+    print_list(playlist);
+    printf("-----------\n");
+
+    printf("Testing free the entire list and printing the resulting list: (Expecting Nothing)\n");
+    playlist = free_list(playlist);
+    print_list(playlist);
+    printf("-----------\n");
+
+}
+
+int oldTesting(){
     srand( time(NULL) );
     // struct node * front = insert_front( '\0',"Bohemian Rhapsody", "Queen");
     // front = insert_front( front,"Build Me Up Buttercup", "The Foundations");
